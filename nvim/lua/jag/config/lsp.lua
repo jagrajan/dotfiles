277959aaba51f0b3a1e7b967cc3cbec16241a828
Jagrajan Bhullar <jag@jagrajan.com>
2021-04-05 11:20:52 -0700

    [nvim] remove completion.nvim on_attach

--- nvim/lua/jag/config/lsp.lua
+++ nvim/lua/jag/config/lsp.lua
@@ -1,13 +1,11 @@
 local nvim_lsp = require('lspconfig')
 
-local on_attach = require'completion'.on_attach
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- local servers = {'cssls', 'bashls', 'diagnosticls', 'dockerls', 'flow', 'ghcide', 'gopls', 'hie', 'html', 'intelephense', 'tsserver', 'jsonls', 'kotlin_language_server', 'pyls', 'rls', 'rust_analyzer', 'sourcekit', 'vimls', 'vuels'}
 local servers = {'tsserver'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
-    on_attach = on_attach,
     capabilities = capabilities,
   }
 end
